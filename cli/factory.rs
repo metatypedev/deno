2eed7f02a4fae71d6252a1468b11abebc9e6d668
Teo Stocco <teo@zifeo.com>
2024-01-22 23:54:54 +0100

    feat: lib

--- cli/factory.rs
+++ cli/factory.rs
@@ -52,6 +52,8 @@ use crate::util::progress_bar::ProgressBar;
 use crate::util::progress_bar::ProgressBarStyle;
 use crate::worker::CliMainWorkerFactory;
 use crate::worker::CliMainWorkerOptions;
+use crate::worker::CustomExtensionsCb;
+use crate::worker::CustomSnapshotCb;
 use std::path::PathBuf;
 
 use deno_core::error::AnyError;
@@ -100,6 +102,8 @@ impl CliFactoryBuilder {
       watcher_communicator: self.watcher_communicator,
       options,
       services: Default::default(),
+      custom_extensions_cb: None,
+      custom_snapshot_cb: None,
     }
   }
 }
@@ -184,6 +188,8 @@ pub struct CliFactory {
   watcher_communicator: Option<Arc<WatcherCommunicator>>,
   options: Arc<CliOptions>,
   services: CliFactoryServices,
+  custom_extensions_cb: Option<Arc<CustomExtensionsCb>>,
+  custom_snapshot_cb: Option<Arc<CustomSnapshotCb>>,
 }
 
 impl CliFactory {
@@ -193,6 +199,20 @@ impl CliFactory {
 
   pub fn from_cli_options(options: Arc<CliOptions>) -> Self {
     CliFactoryBuilder::new().build_from_cli_options(options)
+  }
+
+  pub fn with_custom_ext_cb(self, cb: Arc<CustomExtensionsCb>) -> Self {
+    Self {
+      custom_extensions_cb: Some(cb),
+      ..self
+    }
+  }
+
+  pub fn with_custom_snapshot_cb(self, cb: Arc<CustomSnapshotCb>) -> Self {
+    Self {
+      custom_snapshot_cb: Some(cb),
+      ..self
+    }
   }
 
   pub fn cli_options(&self) -> &Arc<CliOptions> {
@@ -891,6 +911,8 @@ impl CliFactory {
       maybe_root_package_json_deps: self.options.maybe_package_json_deps(),
       create_hmr_runner,
       create_coverage_collector,
+      custom_extensions_cb: self.custom_extensions_cb.clone(),
+      custom_snapshot_cb: self.custom_snapshot_cb.clone(),
     })
   }
 }
